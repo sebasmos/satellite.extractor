a18b3f9e0244376ea69cce749407f59b6d2adc1b
sebasmos <ulsordonez@unicauca.edu.co>
2022-10-13 06:02:49 +0000

    updating scripts

--- src/hash-encryption/config.py
+++ src/hash-encryption/config.py
@@ -1,8 +1,9 @@
 # config.py
-DATASET = "/home/sebasmos/Desktop/DATASETS/DATASET_5_best_cities"
+#DATASET = "/home/sebasmos/Desktop/DATASETS/DATASET_5_best_cities"
+DATASET = "/home/datascience/DATASET_5_best_cities"
 image_size= 750
-resize_ratio=(0.05, 0.05, 1)
-resizing = False
+resize_ratio=(1, 1, 1)#(0.05, 0.05, 1)
+resizing = True 
 normalize = True
 printing = False
 visualization = "data"
